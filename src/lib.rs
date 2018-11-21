38fa14b1cf527c0ee43848f750b60066864583bc
tib <orb8@freemail.hu>
2018-11-22 00:42:05 +0100

    hx1230 module support added

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,9 @@ extern crate nb;
 // TODO: feature flag
 extern crate fpa;
 
-mod font;
-mod hx1230;
-mod pcd8544;
-
-pub use pcd8544::gpio::Pcd8544Gpio;
-pub use pcd8544::spi::Pcd8544Spi;
-pub use pcd8544::Modes::*;
-
-pub use hx1230::gpio::Hx1230Gpio;
-pub use hx1230::Modes::*;
+pub mod font;
+pub mod hx1230;
+pub mod pcd8544;
 
 pub trait Display {
     /// x must be 0..83
